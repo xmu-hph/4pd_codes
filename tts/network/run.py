95ffbc778229bf6a91789aa21659c17662e0be2a
hupenghui <hupenghui>
2024-07-26 10:35:43 +0000

    new

--- tts/network/run.py
+++ tts/network/run.py
@@ -1,6 +1,17 @@
 import torch
 import torch.nn as nn
 import time
+from loguru import logger
+import argparse
+#nohup python run.py --png_save_path run1 >run1.log 2>&1 &
+parser = argparse.ArgumentParser()
+parser.add_argument('--png_save_path',type=str,default='debug')
+args = parser.parse_args()
+import os
+if os.path.exists(args.png_save_path):
+    logger.info(f"file save path: ./{args.png_save_path}")
+else:
+    os.makedirs(args.png_save_path)
 class SimpleNN(nn.Module):
     def __init__(self, nums=100, max_freq=12000):
         super(SimpleNN, self).__init__()
@@ -13,7 +24,6 @@ class SimpleNN(nn.Module):
         # omega and alpha are now fixed but defined based on the initialized parameters
         self.register_buffer('omega', max_freq / torch.arange(1, nums + 1, dtype=torch.float).reshape(1, -1))
         self.register_buffer('alpha', self.omega / 10000)
-        
         self.start_time = nn.Parameter(torch.randn(1))
     
     def forward(self, t):
@@ -26,7 +36,7 @@ class SimpleNN(nn.Module):
         return result
 
 class WordVoice(nn.Module):
-    def __init__(self, word=13, nums=100, max_freq=12000):
+    def __init__(self, word=2, nums=100, max_freq=12000):
         super(WordVoice, self).__init__()
         self.word = word
         self.net = nn.ModuleList([SimpleNN(nums, max_freq) for _ in range(word)])
@@ -34,7 +44,7 @@ class WordVoice(nn.Module):
     def forward(self, t):
         out = torch.stack([net(t) for net in self.net]).sum(dim=0)
         return out
-from loguru import logger
+
 #使用sf读取wav文件
 import soundfile as sf
 # 指定要读取的wav文件路径
@@ -55,12 +65,13 @@ t = np.array(list(range(len(data))))*T
 x_t = data
 # 绘制时间域信号 x_discrete(t)
 plt.figure(figsize=(10, 4))
-plt.stem(t[11000:12000], x_t[11000:12000], basefmt=" ")
+plt.stem(t[4410:6615], x_t[4410:6615], basefmt=" ")
 plt.title('$x_{discrete}(t)$')
 plt.xlabel('Time [s]')
 plt.ylabel('Amplitude')
 plt.grid()
-plt.savefig(f'Amplitude_Time.png', dpi=300, bbox_inches='tight')
+plt.savefig(f'./{args.png_save_path}/Amplitude_Time.png', dpi=300, bbox_inches='tight')
+plt.close()
 #plt.show()
 import numpy as np
 import torch
@@ -70,7 +81,7 @@ import matplotlib.pyplot as plt
 if __name__=='__main__':
     # 检查是否有可用的 GPU
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = WordVoice().to(device)
+    model = WordVoice(word=2, nums=100, max_freq=12000).to(device)
     x = t.reshape(-1,1).astype(np.float32)
     y = x_t.reshape(-1,1).astype(np.float32)
     x_tensor = torch.from_numpy(x).to(device)
@@ -89,10 +100,15 @@ if __name__=='__main__':
         optimizer.step()
         if (epoch+1) % 400 == 0:
             plt.figure(figsize=(10, 4))
-            plt.plot(x[11000:12000], y[11000:12000], label='True')
-            plt.plot(x[11000:12000], outputs.detach().cpu().numpy()[11000:12000], label='Predicted')
+            plt.title('$x_{discrete}(t)$')
+            plt.xlabel('Time [s]')
+            plt.ylabel('Amplitude')
+            plt.plot(x[4410:6615], y[4410:6615], label='True')
+            plt.plot(x[4410:6615], outputs.detach().cpu().numpy()[4410:6615], label='Predicted')
             plt.legend()
-            plt.savefig(f'Predict_Amplitude_Time_{epoch+1}.png', dpi=300, bbox_inches='tight')
+            plt.grid()
+            plt.savefig(f'./{args.png_save_path}/Predict_Amplitude_Time_{epoch+1}.png', dpi=300, bbox_inches='tight')
+            plt.close()
             #plt.show()
             logger.info(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}, Time: {time.time()-start_time}s')
         start_time = time.time()
@@ -101,10 +117,15 @@ if __name__=='__main__':
     predicted = model(x_tensor).detach().cpu().numpy()
     # 绘制结果
     plt.figure(figsize=(10, 4))
+    plt.title('$x_{discrete}(t)$')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Amplitude')
     plt.plot(x, y, label='True')
     plt.plot(x, predicted, label='Predicted')
     plt.legend()
-    plt.savefig(f'Predict_Amplitude_Time.png', dpi=300, bbox_inches='tight')
+    plt.grid()
+    plt.savefig(f'./{args.png_save_path}/Predict_Amplitude_Time.png', dpi=300, bbox_inches='tight')
+    plt.close()
     #plt.show()
     #for key in model.parameters():
     #    print(key)
