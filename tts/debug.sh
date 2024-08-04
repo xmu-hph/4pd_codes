18fa060e66ab1fea975904ae01a83e1b4f4850c2
hupenghui <hupenghui>
2024-08-04 12:54:04 +0000

    tts code

--- tts/debug.sh
+++ tts/debug.sh
@@ -3,23 +3,9 @@ docker run --rm -it --gpus '"device=6"' --network host \
 -e LD_PRELOAD=/usr/local/vgpu/libvgpu.so \
 -v /home/hupenghui/vgpu/libvgpu.so:/usr/local/vgpu/libvgpu.so \
 -v /home/hupenghui/vgpulock:/tmp/vgpulock \
-<<<<<<< HEAD
-<<<<<<< HEAD
-harbor-contest.4pd.io/hupenghui/tts:tts_stream_clone_1655 /bin/bash
+-e CUDA_DEVICE_SM_LIMIT=10 \
+-e GPU_CORE_UTILIZATION_POLICY=force \
+harbor-contest.4pd.io/hupenghui/tts:tts_stream_clone_without_zero_res /bin/bash
 #-v /mnt/data/hupenghui/model/tts_models--multilingual--multi-dataset--xtts_v2:/root/model/tts \
 #-v /mnt/data/hupenghui/model/zh_core_web_sm-3.7.0/zh_core_web_sm/zh_core_web_sm-3.7.0:/root/model/zh \
 #-v /mnt/data/hupenghui/model/en_core_web_sm-3.7.1/en_core_web_sm/en_core_web_sm-3.7.1:/root/model/en \
-=======
--v /mnt/data/hupenghui/model/tts_models--multilingual--multi-dataset--xtts_v2:/root/model/tts \
--v /mnt/data/hupenghui/model/zh_core_web_sm-3.7.0/zh_core_web_sm/zh_core_web_sm-3.7.0:/root/model/zh \
--v /mnt/data/hupenghui/model/en_core_web_sm-3.7.1/en_core_web_sm/en_core_web_sm-3.7.1:/root/model/en \
-harbor-contest.4pd.io/hupenghui/tts:tts_stream_clone_1655 /bin/bash
->>>>>>> parent of 16fe679 (remove first null response)
-=======
--v /mnt/data/hupenghui/model/tts_models--multilingual--multi-dataset--xtts_v2:/root/model/tts \
--v /mnt/data/hupenghui/model/zh_core_web_sm-3.7.0/zh_core_web_sm/zh_core_web_sm-3.7.0:/root/model/zh \
--v /mnt/data/hupenghui/model/en_core_web_sm-3.7.1/en_core_web_sm/en_core_web_sm-3.7.1:/root/model/en \
-harbor-contest.4pd.io/hupenghui/tts:tts_stream_clone_without_zero_res /bin/bash
->>>>>>> parent of f57c1a6 (new)
-
-#uvicorn.run("app:app", host="0.0.0.0", port=80)
